244cd54564c28982cac9c085386bd3eb03d5240a
Access Point / Javier Reyes Herrera <sw1027rp@go.minnstate.edu>
2025-10-07 15:46:51 -0500

    first commit

---
+++ order_status.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+# ORDER_Status = '123456654321'
+
+ORDER_STATUS_KEY = os.environ.get('ORDER_STATUS_KEY')
+print(ORDER_STATUS_KEY)
+order_number = input('Enter your order number: ') # You can make up a number
+
+url = f'https://mock-order-status.uc.r.appspot.com/orders/status/{order_number}?API_KEY={ORDER_STATUS_KEY}'
+response = requests.get(url).json()
+print(response)
+
+status = response['status']
+print(f'Order status: {status}')
+
+
+# TODO - can you print the order number?
+# TODO - can you print the order's items?
